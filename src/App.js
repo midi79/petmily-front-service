c329d74f90149402671750f8d833c5ec84226ae1
Anderson Lee <midi279@gmail.com>
2021-06-01 22:57:24 +0900

    add a sort of material samples

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Button from '@material-ui/core/Button'
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Checkbox from '@material-ui/core/Checkbox'
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <div>
+      <Checkbox 
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+        inputProps={{
+          'arial-label': 'secondary checkbox'
+        }}
+      />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CheckboxExample />
+        <ButtonGroup variant="contained" color="primary">
+          <Button
+            startIcon={<SaveIcon />}          
+            >
+              Save
+          </Button>
+          <Button 
+            startIcon={<DeleteIcon />}          
+            >
+              Discard
+          </Button>
+        </ButtonGroup>
+        <img src={logo} className="App-logo" alt="logo" />        
       </header>
     </div>
   );
