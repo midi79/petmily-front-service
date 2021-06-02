01fb404bf9568039c1d038cdc1b60626d0b7fb3d
Anderson Lee <midi279@gmail.com>
2021-06-02 23:07:51 +0900

    sample added

--- src/App.js
+++ src/App.js
@@ -6,42 +6,135 @@ import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
 import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import TextField from '@material-ui/core/TextField'
+
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { orange, green } from '@material-ui/core/colors';
+
+import 'fontsource-roboto';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FFBBFF, #FF11FF)',
+    board: 0,
+    marginBottom: 15,
+    boarderRadius: 15,
+    color: 'white',
+    padding: '0 30px'
+  }
+})
+
+const theme = createMuiTheme({
+  typography: {
+    h2: {
+      fontSize: 36,
+      marginBottom: 15,
+    }
+  },
+  palette: {
+    primary: {
+      main: orange[400],
+    },
+    secondary: {
+      main: green[400],
+    }
+  }
+})
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
 
 function CheckboxExample() {
   const [checked, setChecked] = React.useState(true);
   return (
     <div>
-      <Checkbox 
-        checked={checked}
-        onChange={(e) => setChecked(e.target.checked)}
-        inputProps={{
-          'arial-label': 'secondary checkbox'
-        }}
-      />
+      <FormControlLabel 
+        control={<Checkbox 
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          inputProps={{
+            'arial-label': 'secondary checkbox'
+          }}
+        />}
+        label="Test Checkbox"
+      />      
     </div>
   )
 }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <CheckboxExample />
-        <ButtonGroup variant="contained" color="primary">
-          <Button
-            startIcon={<SaveIcon />}          
-            >
-              Save
-          </Button>
-          <Button 
-            startIcon={<DeleteIcon />}          
-            >
-              Discard
-          </Button>
-        </ButtonGroup>
-        <img src={logo} className="App-logo" alt="logo" />        
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="xs">
+        <div className="App">
+          <header className="App-header">
+            <AppBar color="secondary">
+              <ToolBar>
+                <IconButton>
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">
+                  MUI Themeing!!
+                </Typography>
+                <Button>
+                  Login
+                </Button>
+              </ToolBar>
+            </AppBar>
+            <Typography variant="h2">
+              Welcom to MUI!
+            </Typography>
+            <Typography variant="subtitle1">
+              Learn how to use Material UI
+            </Typography>
+            <ButtonStyled />
+            <Grid container spacing={2} justify="center">
+              <Grid item xs={3} sm={6}>
+                <Paper style={{ height:75, width: '100%', }}/>
+              </Grid>
+              <Grid item xs={3} sm={6}>
+                <Paper style={{ height:75, width: '100%', }}/>
+              </Grid>
+              <Grid item xs={3} sm={6}>
+                <Paper style={{ height:75, width: '100%', }}/>
+              </Grid>
+            </Grid>
+            <TextField 
+              variant="outlined"
+              color="secondary"
+              type="date"
+              label="Date"
+            />
+            <CheckboxExample />
+            <ButtonGroup variant="contained" color="primary">
+              <Button
+                startIcon={<SaveIcon />}          
+                >
+                  Save
+              </Button>
+              <Button 
+                startIcon={<DeleteIcon />}          
+                >
+                  Discard
+              </Button>
+            </ButtonGroup>
+            <img src={logo} className="App-logo" alt="logo" />        
+          </header>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
 
